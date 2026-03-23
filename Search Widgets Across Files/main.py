dc5733743d8038fe86fe11ec361e3a621d01aff2
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2026-03-23 16:33:09 -0400

    Create main.py

---
+++ Search Widgets Across Files/main.py
@@ -0,0 +1,12 @@
+"""
+Search Widgets Across Files 
+Author: Andrew R Sutton
+Created: March 23, 2026
+
+The purpose of this project is to demonstrate how to 
+search text in a tkinter text widget from another Python 
+file inside the same project?
+
+In short, this is an exercise in how to keep projects 
+appropriately modular.
+"""
